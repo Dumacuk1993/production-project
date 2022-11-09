2c8e2337712e8bb11fa7b4cdb77097cabba8f21a
Dmitry K <dimakuryndin1@yandex.ru>
2022-11-09 21:30:04 +0300

    test 10

--- src/app/providers/ThemeProvider/index.ts
+++ src/app/providers/ThemeProvider/index.ts
@@ -1,9 +1,9 @@
-import { useTheme } from './lib/useTheme';
 import ThemeProvider from './ui/ThemeProvider';
+import { useTheme } from './lib/useTheme';
 import { Theme } from './lib/ThemeContext';
 
 export {
     ThemeProvider,
-    Theme,
     useTheme,
+    Theme,
 };
